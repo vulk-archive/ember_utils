646bc40729f34037faadc565237da58e76579686
wolfpack <wolfpack@vulk.co>
2016-03-25 04:40:50 -0400

    logging params on failure now enabled

--- on-failure-404-ok.js
+++ on-failure-404-ok.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 export default function onFailure404Ok(params/*, hash*/) {
   console.log('helper onFailure404Ok');
+  console.log('helper onFailure404Ok params: ' + params);
   if (params.status === 500){
     return alert('Internal server error. Contact administrator');
   }  else if (params.status === 404) {
