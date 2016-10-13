c1dcaefeee2b9e97db0432d2505853bd6275eb2f
wolfpack <wolfpack@vulk.co>
2016-10-13 12:56:04 -0400

    add object and replace object utilties now available

---
+++ on-success-add-object.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default function onSuccessAddObject(model_name) {
+  console.log('helper onSuccessAddObject');
+  var ret_fun = function(params/*, hash*/) {
+    if (params.get('length') > 0){
+        return model_name.addObject(params);
+    }
+  }.bind(this);
+  return ret_fun;
+}
+
---
+++ on-success-replace-object.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default function onSuccessReplaceObject(model_name) {
+  console.log('helper onSuccessReplaceObject');
+  var ret_fun = function(params/*, hash*/) {
+    if (params.get('length') > 0){
+        return model_name.set('content', params);
+    }
+  }.bind(this);
+  return ret_fun;
+}
+
